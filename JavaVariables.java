42d60196071df91189b2aaf49232a87798ab72af
Kishore Ram .G.M <124455749+Ram200110@users.noreply.github.com>
2024-12-16 17:03:11 +0530

    day 1 part 2

---
+++ JavaVariables.java
@@ -0,0 +1,28 @@
+public class JavaVariables
+{
+public static void main(String args[])
+{
+	int num= 10;
+System.out.println(num);
+
+	String name= "kishore";
+System.out.println(name);
+
+System.out.println("String : "+name);
+
+	float  rupees= 5.7f;
+System.out.println(rupees);
+
+	char a ='a';
+System.out.println(a);
+
+	double b =5.6666;
+System.out.println(b);
+
+	boolean c =true;
+System.out.println(c);
+
+
+
+}
+}
