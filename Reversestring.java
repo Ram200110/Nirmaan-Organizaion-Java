bf6f36616e2f6b2a27753f87956d0d4331930327
Kishore Ram .G.M <124455749+Ram200110@users.noreply.github.com>
2024-12-30 17:16:55 +0530

    day6

---
+++ Reversestring.java
@@ -0,0 +1,16 @@
+package day6;
+
+public class Reversestring {
+public static void main(String[] args) {
+//	String name="kishore";
+//	System.out.println(name.toUpperCase()); 
+//	String re ="";
+//	for(int i=6;i>=0;i--) {
+//		re += name.charAt(i);
+//		System.out.print(name.charAt(i));
+//	}
+//	System.out.println();
+//	
+//	System.out.println(re.toUpperCase());		
+}
+}
