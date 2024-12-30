f0034cebf4140c56828c15d235167c14624414ca
Kishore Ram .G.M <124455749+Ram200110@users.noreply.github.com>
2024-12-30 17:16:18 +0530

    day6

---
+++ array.java
@@ -0,0 +1,13 @@
+package day6;
+
+public class array {
+	
+	public static void main(String[] args) {
+		String fruits[] = {"Apple","Banana","Grape","Mango"};
+		
+		for(int i=0;i<fruits.length;i++) {
+		System.out.println(fruits[i]);
+	}
+
+}
+}
