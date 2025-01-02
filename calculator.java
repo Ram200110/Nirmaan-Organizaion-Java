b51be0549e61b451403fd3ab8bcbe1e0c9863fb8
Kishore Ram .G.M <124455749+Ram200110@users.noreply.github.com>
2025-01-02 15:28:00 +0530

    day8

---
+++ calculator.java
@@ -0,0 +1,24 @@
+package day8;
+
+public class calculator {
+int add(int a, int b) {
+	return (a+b);
+}
+
+int sub(int a, int b) {
+	return(a-b);
+	
+}
+
+int mul(int a,int b) {
+	return(a*b);
+}
+int div(int a,int b) {
+	return(a/b);
+}
+
+int modul(int a,int b) {
+	return(a%b);
+}
+
+}
